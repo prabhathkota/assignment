f7f0be1dd81f08c0f682806ebdf9f212bc219fe6
Prabhath Kota <prabhathkota@gmail.com>
2019-01-17 20:30:07 +0530

    Fixing issue & changed tab to space
    Fixed the issue & changed tab to space

--- prabhath.py
+++ prabhath.py
@@ -7,6 +7,7 @@
 #               :   cat input.txt | prabhath.py > output.txt (Linux)
 #/*********************************************/
 import sys
+import re
 
 star_dict = {}
 dot_prev_line = ''
@@ -40,7 +41,6 @@ def get_dot_val(each_line):
   global dot_prev_count
   global dot_prev_line
   if not dot_prev_count:
-    pass
     dot_prev_count = dot_count
     dot_prev_line = ''
     dot_str += '-'
@@ -73,13 +73,14 @@ def get_star_val(each_line):
   return start_val
 
 arr = []
-prev_dot_val = ''
+prev_dot_val = 0
 for each in sys.stdin:
   if len(each) == 0:
     continue
   each_line = each.strip()
   if each_line.startswith('*'):
     reset_prev_dot_data()
+    prev_dot_val = 0
     start_val = get_star_val(each_line)
     arr.append(str(start_val) + each_line.replace('*', ''))
   elif each_line.startswith('.'):
@@ -89,17 +90,34 @@ for each in sys.stdin:
     arr.append(str(dot_val) + each_line.replace('.', ''))
   else:
     if prev_dot_val:
-      arr.append('\t'*prev_dot_val + '  ' + each_line)
+      last_element = arr[-1]
+      last_element = last_element + '\n' + '\t'*prev_dot_val + '  ' + each_line
+      arr[-1] = last_element 
+    else:
+      last_element = arr[-1]
+      last_element = last_element + '\n' + each_line
+      arr[-1] = last_element 
+
+#print arr
 
 for x, y in zip(arr, arr[1:]):
-  count_tabs_in_x = x.count('\t') 
-  count_tabs_in_y = y.count('\t')
-  #if count_tabs_in_x:
+  count_tabs_in_x = 0
+  count_tabs_in_y = 0
+  #Count tabs starting with
+  pattern = re.compile(r'^(\t*)', re.IGNORECASE)
+  res = re.match(pattern, x)  
+  if (len(res.groups())):
+    count_tabs_in_x = res.group(1).count('\t')
+  res_y = re.match(pattern, y)  
+  if (len(res.groups())):
+    count_tabs_in_y = res_y.group(1).count('\t')
+
   if count_tabs_in_y > count_tabs_in_x:
       x = x.replace('-', '+')
   else:
       x = x.replace('+', '-')
+  #Replacing tabs to spaces
+  x = x.replace('\t', ' ')
   print x
-print arr[-1]
-
+print arr[-1].replace('\t', ' ')
 

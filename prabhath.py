cef136309b4bf1f5f42d8cac24faa212c88dda49
Prabhath Kota <prabhathkota@gmail.com>
2019-01-17 21:40:44 +0530

    Added try catch blocks
    Added try catch blocks

--- prabhath.py
+++ prabhath.py
@@ -72,58 +72,70 @@ def get_star_val(each_line):
     start_val = start_val.rstrip('.')
   return start_val
 
-arr = []
-prev_dot_val = 0
-for each in sys.stdin:
-  if len(each) == 0:
-    continue
-  each_line = each.strip()
-  if each_line.startswith('*'):
-    reset_prev_dot_data()
-    prev_dot_val = 0
-    start_val = get_star_val(each_line)
-    arr.append(str(start_val) + each_line.replace('*', ''))
-  elif each_line.startswith('.'):
-    dot_val = get_dot_val(each_line)
-    dot_count = count_dots(each_line)
-    prev_dot_val = dot_count
-    arr.append(str(dot_val) + each_line.replace('.', ''))
-  else:
-    if prev_dot_val:
-      last_element = arr[-1]
-      last_element = last_element + '\n' + '\t'*prev_dot_val + '   ' + each_line
-      arr[-1] = last_element 
+def display_output(arr):
+  try:
+    if not len(arr):
+      print 'No data to display, something wrong...'
+      sys.exit()
+    for x, y in zip(arr, arr[1:]):
+      count_tabs_in_x = 0
+      count_tabs_in_y = 0
+      #Count tabs starting with
+      pattern = re.compile(r'^(\t*)', re.IGNORECASE)
+      res = re.match(pattern, x)  
+      if (len(res.groups())):
+        count_tabs_in_x = res.group(1).count('\t')
+      res_y = re.match(pattern, y)  
+      if (len(res.groups())):
+        count_tabs_in_y = res_y.group(1).count('\t')
+
+      if count_tabs_in_y > count_tabs_in_x:
+          x = x.replace('-', '+')
+      else:
+          x = x.replace('+', '-')
+
+      if not x[0].isdigit():
+        x = ' ' + x
+      #Replacing tabs to spaces
+      x = x.replace('\t', ' ')
+      print x
+
+    last_element = arr[-1]
+    if not last_element[0].isdigit():
+      last_element = ' ' + last_element
+      last_element = last_element.replace('\t', ' ')
+      #arr[-1] = last_element
+    print last_element
+  except Exception, e:
+    print 'Exception in display_output: ' + str(e)
+
+
+if __name__ == '__main__':
+  arr = []
+  prev_dot_val = 0
+  for each in sys.stdin:
+    if len(each) == 0:
+      continue
+    each_line = each.strip()
+    if each_line.startswith('*'):
+      reset_prev_dot_data()
+      prev_dot_val = 0
+      start_val = get_star_val(each_line)
+      arr.append(str(start_val) + each_line.replace('*', ''))
+    elif each_line.startswith('.'):
+      dot_val = get_dot_val(each_line)
+      dot_count = count_dots(each_line)
+      prev_dot_val = dot_count
+      arr.append(str(dot_val) + each_line.replace('.', ''))
     else:
-      last_element = arr[-1]
-      last_element = last_element + '\n' + each_line
-      arr[-1] = last_element 
+      if prev_dot_val:
+        prev_element = arr[-1]
+        prev_element = prev_element + '\n' + '\t'*prev_dot_val + '   ' + each_line
+        arr[-1] = prev_element 
+      else:
+        prev_element = arr[-1]
+        prev_element = prev_element + '\n' + each_line
+        arr[-1] = prev_element 
 
-#print arr
-
-for x, y in zip(arr, arr[1:]):
-  count_tabs_in_x = 0
-  count_tabs_in_y = 0
-  #Count tabs starting with
-  pattern = re.compile(r'^(\t*)', re.IGNORECASE)
-  res = re.match(pattern, x)  
-  if (len(res.groups())):
-    count_tabs_in_x = res.group(1).count('\t')
-  res_y = re.match(pattern, y)  
-  if (len(res.groups())):
-    count_tabs_in_y = res_y.group(1).count('\t')
-
-  if count_tabs_in_y > count_tabs_in_x:
-      x = x.replace('-', '+')
-  else:
-      x = x.replace('+', '-')
-
-  if not x[0].isdigit():
-    x = ' ' + x
-  #Replacing tabs to spaces
-  x = x.replace('\t', ' ')
-  print x
-
-if not arr[-1].isdigit():
-  arr[-1] = ' ' + arr[-1] 
-print arr[-1].replace('\t', ' ')
-
+  #print arr
+  display_output(arr)
